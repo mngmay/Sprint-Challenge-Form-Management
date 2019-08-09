3cf558caac64cc472e93692e69d885992b0224df
May Ng <mngmay@gmail.com>
2019-08-09 09:09:36 -0700

    GET call and set recipes from api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 import React from "react";
+import axios from "axios";
 import FormikRegistrationForm from "./components/RegistrationForm";
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    users: [],
+    passwords: [],
+    recipes: []
+  };
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  getRecipes = () => {
+    axios
+      .get(`http://localhost:5000/api/restricted/data`)
+      .then(recipes => {
+        recipes.data.forEach(recipe =>
+          this.setState({ recipes: [...this.state.recipes, recipe] })
+        );
+        console.log("Recipes State", this.state.recipes);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
       <div className="App">
-        <FormikRegistrationForm />
+        <FormikRegistrationForm users={this.state.users} />
       </div>
     );
   }
